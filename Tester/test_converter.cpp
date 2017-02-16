64895539e04230e268d65f95f025e6d044b3929b
dominikakk123 <dominika.szymanska@ericsson.com>
2017-02-16 09:47:27 +0100

    Update test_converter.cpp

--- Tester/test_converter.cpp
+++ Tester/test_converter.cpp
@@ -2,23 +2,11 @@
 #include <vector>
 #include "../GameOfLife/converterboolboardtostring.h"
 
-TEST_CASE("check in convert to string properly","[Converter]"){
-//std::vector<std::vector<bool>>temporary;
-//{
-//    for(int=0; i<4; i++){
-//      temporary.push_back(rand()%2);
-//    }
-//
-    std::string convertedString;
+TEST_CASE("check if converting to string is properly","[Converter]"){
+
+std::string convertedString;
 std::vector<std::vector<bool>> temporary(4, std::vector<bool>(4));
 
-//for(int i=1; i<3; i++){
-//    for(int j=1;j<3;j++){
-//        temporary[i][j] = rand()%2;
-//    }
-//}
-
-//vector
 temporary[1][1]=1;
 temporary[1][2]=1;
 temporary[2][1]=0;
@@ -26,12 +14,12 @@ temporary[2][2]=0;
 
 ConverterBoolBoardToString *converter;
 converter = new ConverterBoolBoardToString;
-//convertedString = ;
+
 
 CHECK(converter->convertBoolBoardToString(temporary).size()== 6);
 
 
-//CHECK(converter->convertBoolBoardToString(temporary)[0]=='.'); // nie zadziala
+
 CHECK(converter->convertBoolBoardToString(temporary)[0]=='x');
 CHECK(converter->convertBoolBoardToString(temporary)[1]=='x');
 CHECK(converter->convertBoolBoardToString(temporary)[2]=='\n');
@@ -41,7 +29,6 @@ CHECK(converter->convertBoolBoardToString(temporary)[5]=='\n');
 
 temporary.clear();
 
-std::cout<<temporary[1][1];
 delete converter;
 }
 
